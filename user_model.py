cbe2594dbb60f687a9185ff032d9a776f33cf293
Samar Ali <samarali27698@gmail.com>
2025-07-03 01:54:25 +0300

    Added Student subclass with course enrollment method

--- user_model.py
+++ user_model.py
@@ -11,4 +11,17 @@ class User:
 
     def set_email(self, new_email):
         self.__email = new_email
-        
+
+#student subclass inheriting from user class
+class Student(User):
+    def __init__(self, name, email):
+        super().__init__(name, email)
+        self.__enrolled_courses = []
+
+    def enroll(self, course_name):
+        if course_name not in self.__enrolled_courses:
+            self.__enrolled_courses.append(course_name)
+            print(f"Enrolled in {course_name}")
+        else:
+            print(f"Already enrolled in {course_name}")
+
