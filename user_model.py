7bab1cb010c480bfe20f2aa75285567f7b234c6f
Samar Ali <samarali27698@gmail.com>
2025-07-03 01:58:12 +0300

    Added Instructor subclass with course assignment method

--- user_model.py
+++ user_model.py
@@ -25,3 +25,27 @@ class Student(User):
         else:
             print(f"Already enrolled in {course_name}")
 
+#instructor subclass inheriting from user class
+class Instructor(User):
+    def __init__(self, name, email):
+        super().__init__(name, email)
+        self.__teaching_courses = []
+
+    def add_course(self, course_name):
+        if course_name not in self.__teaching_courses:
+            self.__teaching_courses.append(course_name)
+            print(f"Added {course_name} to teaching courses")
+        else:
+            print(f"Already teaching {course_name}")
+
+
+student1 = Student("Aya", "aya@gmail.com")
+student1.enroll("Calculus1")
+student1.enroll("Physics")
+student1.enroll("Calculus1")
+
+
+instructor1= Instructor("Taha", "taha@gmail.com")
+instructor1.add_course("Functions")
+instructor1.add_course("Inheritance")
+instructor1.add_course("Functions")
